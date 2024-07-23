18a36cafef21eeb1c3a6648560e1028ce74ee021
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-23 09:32:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ api_id = 'YOUR_API_ID'
 api_hash = 'YOUR_API_HASH'
 bot_token = 'YOUR_BOT_TOKEN'
 
-app = Client("audio_extractor_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+app = Client("audio_extractor_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 flask_app = Flask(__name__)
 
 DOWNLOAD_DIR = "downloads"
