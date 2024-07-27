861e8b50572f4312116113867855f0d11e8ec22e
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-27 14:46:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,6 @@ API_ID = os.getenv('API_ID', '21740783')
 API_HASH = os.getenv('API_HASH', 'a5dc7fec8302615f5b441ec5e238cd46')
 BOT_TOKEN = os.getenv('BOT_TOKEN', '7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU')
 OWNER_USERNAME = '@speedwolf'
-IS_PREMIUM = False  # Set to True if you have a premium account
+# Directory paths
+VIDEO_DIR = 'videos/'
+AUDIO_DIR = 'audios/'
