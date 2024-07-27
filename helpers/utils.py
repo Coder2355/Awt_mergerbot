01257f4de38c3dac7d5ea1b19be7229790e01672
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-27 11:43:02 +0530

    Create utils.py

---
+++ helpers/utils.py
@@ -0,0 +1,9 @@
+class UserSettings:
+    def __init__(self, user_id, first_name):
+        self.user_id = user_id
+        self.first_name = first_name
+        self.thumbnail = None  # Placeholder for actual thumbnail retrieval
+
+def delete_all(root):
+    # Function to delete all files in a directory
+    pass
